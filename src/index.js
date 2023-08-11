3468fd4a2e20e976e1cadd84edcba30f1a655d9c
Ernest Ho <ernest.ho.001@gmail.com>
2023-08-11 12:54:05 -0400

    Created Vue Project

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppContainer from './AppContainer';
-import './Styles/html.scss'
-
-ReactDOM.render(<AppContainer />, document.getElementById("root"));
